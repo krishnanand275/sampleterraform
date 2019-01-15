428c39a7da04d1194fa3341e773c4ab5d2b0add3
krishna.c <krishna.c@edgenetworks.in>
2019-01-15 14:30:11 +0530

    sample teraaform project

---
+++ main.tf
@@ -0,0 +1,7 @@
+output "value1" {
+  value = "${var.var1}"
+}
+
+output "value2" {
+  value = "${var.var2}"
+}
---
+++ variable.tf
@@ -0,0 +1,9 @@
+variable "var1" {
+  type    = "string"
+  default = "value"
+}
+
+variable "var2" {
+  type    = "string"
+  default = "value"
+}
